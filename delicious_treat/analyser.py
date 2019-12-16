e39af3860cba4407962b3c98dc8c20a0caf32bea
briggySmalls <briggySmalls90@gmail.com>
2019-12-16 08:25:40 +0000

    Big rewrite to lemmatise properly

--- delicious_treat/analyser.py
+++ delicious_treat/analyser.py
@@ -1,52 +1,74 @@
 """Analysis of a set of text"""
-from typing import Optional, Sequence
+from typing import Sequence
+from string import punctuation
 
-from nltk.probability import FreqDist
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk import pos_tag
 import pandas as pd
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet as wn
+from nltk.probability import ConditionalFreqDist, FreqDist
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 
-class Analyser:
-    """Class for analysing a bulk of text"""
+def _penn2morphy(tag):
+    if tag.startswith('NN'):
+        return wn.NOUN
+    elif tag.startswith('VB'):
+        return wn.VERB
+    elif tag.startswith('JJ'):
+        return wn.ADJ
+    elif tag.startswith('RB'):
+        return wn.ADV
+    else:
+        # Default to noun
+        return wn.NOUN
 
-    def __init__(self, text: str, lemmatise: bool = False) -> None:
-        # Tokenise the text
-        self.tokens = self._tokenise(text)
-        # Lemmatise the text
-        if lemmatise:
-            self.tokens = self._lemmatise(self.tokens)
 
-    def frequency_distribution(self, pos: Optional[str] = None) -> FreqDist:
-        """Get a frequency distribution of tokens
+def _analyse(text: str, lemmatise: bool = False) -> pd.DataFrame:
+    # Convert to tokens
+    tokens = _tokenise(text)
+    # Get part of speech
+    part_of_speech = _part_of_speech(tokens)
+    # Lemmatise, if required
+    if lemmatise:
+        part_of_speech = _lemmatise(part_of_speech)
+    # Build a list of tokens we wish to ignore
+    to_ignore = set(stopwords.words("english"))
+    to_ignore.union(set(punctuation))
+    # Filter the tokens
+    valid_tokens = ~part_of_speech.isin(to_ignore)
+    return part_of_speech[valid_tokens]
 
-        Args:
-            pos (Optional[str], optional): Part of speech to filter by
 
-        Returns:
-            FreqDist: Frequency distribution
-        """
-        # Pick tokens based on pos variable
-        if pos is None:
-            tokens = self.tokens
-        else:
-            # Interpret the tokens in terms of POS
-            pos_tokens = pd.DataFrame(pos_tag(self.tokens),
-                               columns=["token", "type"])
-            # Filter out the specified type
-            tokens = pos_tokens[pos_tokens['type'] == pos]['token']
+def _tokenise(text: str) -> Sequence[str]:
+    # First convert into tokens
+    return [
+        token.lower()
+        for sentence in sent_tokenize(text)
+        for token in word_tokenize(sentence)
+    ]
 
-        # Return frequency distribution
-        return FreqDist(tokens)
 
-    @staticmethod
-    def _tokenise(text: str) -> Sequence[str]:
-        return [
-            token.lower() for token in word_tokenize(text)
-        ]
+def _part_of_speech(tokens: Sequence[str]) -> pd.DataFrame:
+    return pd.DataFrame(pos_tag(tokens), columns=["token", "tag"])
 
-    @staticmethod
-    def _lemmatise(tokens: Sequence[str]) -> Sequence[str]:
-        lemmatizer = WordNetLemmatizer()
-        return [lemmatizer.lemmatize(token) for token in tokens]
+
+def _lemmatise(part_of_speech: pd.DataFrame) -> pd.DataFrame:
+    # Create a lemmatiser
+    lemmer = WordNetLemmatizer()
+
+    def lemmatise_row(row: pd.Series) -> pd.Series:
+        (token, tag) = row
+        new_tag = _penn2morphy(tag)
+        new_token = lemmer.lemmatize(token, pos=new_tag)
+        return pd.Series([new_token, new_tag], index=["token", "tag"])
+    # Lemmatise tokens
+    return part_of_speech.apply(lemmatise_row, axis=1)
+
+
+def _freq_dist(part_of_speech: pd.DataFrame) -> FreqDist:
+    dist = ConditionalFreqDist()
+    for _, (token, tag) in part_of_speech.iterrows():
+        dist[tag][token] += 1
+    return dist
