249be2cdf4b30357fe48b7ede1bb6f7466bae5dc
briggySmalls <briggySmalls90@gmail.com>
2019-12-16 01:34:37 +0000

    Update analyser to code

--- delicious_treat/analyser.py
+++ delicious_treat/analyser.py
@@ -1,8 +1,9 @@
 """Analysis of a set of text"""
-from typing import Optional
+from typing import Optional, Sequence
 
 from nltk.probability import FreqDist
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 import pandas as pd
 
@@ -10,11 +11,12 @@ import pandas as pd
 class Analyser:
     """Class for analysing a bulk of text"""
 
-    def __init__(self, text: str):
+    def __init__(self, text: str, lemmatise: bool = False) -> None:
         # Tokenise the text
-        self.tokens = [
-            token.lower() for token in word_tokenize(text)
-        ]
+        self.tokens = self._tokenise(text)
+        # Lemmatise the text
+        if lemmatise:
+            self.tokens = self._lemmatise(self.tokens)
 
     def frequency_distribution(self, pos: Optional[str] = None) -> FreqDist:
         """Get a frequency distribution of tokens
@@ -23,17 +25,28 @@ class Analyser:
             pos (Optional[str], optional): Part of speech to filter by
 
         Returns:
-            TYPE: Frequency distribution
+            FreqDist: Frequency distribution
         """
         # Pick tokens based on pos variable
         if pos is None:
             tokens = self.tokens
         else:
             # Interpret the tokens in terms of POS
-            pos = pd.DataFrame(pos_tag(self.tokens),
+            pos_tokens = pd.DataFrame(pos_tag(self.tokens),
                                columns=["token", "type"])
             # Filter out the specified type
-            tokens = pos.loc[pos['type'] == pos, 'token']
+            tokens = pos_tokens[pos_tokens['type'] == pos]['token']
 
         # Return frequency distribution
         return FreqDist(tokens)
+
+    @staticmethod
+    def _tokenise(text: str) -> Sequence[str]:
+        return [
+            token.lower() for token in word_tokenize(text)
+        ]
+
+    @staticmethod
+    def _lemmatise(tokens: Sequence[str]) -> Sequence[str]:
+        lemmatizer = WordNetLemmatizer()
+        return [lemmatizer.lemmatize(token) for token in tokens]
